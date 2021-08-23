78d08689dbd4289b0e9ddc003ac038b78da1d66e
Velvet2045 <55654558+Velvet2045@users.noreply.github.com>
2021-08-23 23:27:25 +0900

    Learn SetState
    every time i call setState, js will run render function.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name} :)</h2>
-      <h4>{rating}/5</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image:
-      "https://www.koreanbapsang.com/wp-content/uploads/2019/11/DSC_0831.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "samgyeopsal",
-    image:
-      "https://i.pinimg.com/originals/6c/84/40/6c844009067c1f1301a847f15ee1eaa6.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "jajangmyeon",
-    image:
-      "https://i2.wp.com/seonkyounglongest.com/wp-content/uploads/2017/09/Vegan-Jjajangmyeon-11.jpg?fit=1300%2C730&ssl=1",
-    rating: 4.7,
-  },
-];
-
-function renderFood(dish) {
-  console.log(dish);
-  return (
-    <Food
-      key={dish.key}
-      name={dish.name}
-      picture={dish.image}
-      rating={dish.rating}
-    />
-  );
-}
-
-function App() {
-  return <div>{foodILike.map(renderFood)}</div>;
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
